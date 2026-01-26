63b157adfae4678b100f67fee207d4a5a1e44983
tuyansheng <928532325@qq.com>
2026-01-26 16:06:35 +0800

    代码改好啦！！！今天状态太差了，脑子晕晕的，看了一下，时间反演不是这个意思。所以后面反扩散的结果明显不对，但是我一下子也不知道细致平衡上来说怎么做时间反演。

--- 1D_random_walk_reversal.py
+++ 1D_random_walk_reversal.py
@@ -1,84 +1,56 @@
+# no drift
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(linewidth=300)
 
-# no drift
-
-def transform_matrix():
-    """
-    This is a local transformation matrix for 1D data.
-    Having prob=1/3 go left or right, otherwise stay in place.
-    """
-    mat = np.zeros((n_spatial, n_spatial))
-    for i in range(n_spatial):
-        if i!=0 and i!=n_spatial-1:
-            mat[i, i-1] = 1/3
-            mat[i, i] = 1/3
-            mat[i, i+1] = 1/3
-        elif i==0:
-            mat[i, i] = 1/3
-            mat[i, i+1] = 2/3
-        elif i==n_spatial-1:
-            mat[i, i-1] = 2/3
-            mat[i, i] = 1/3
-    return mat.transpose()
-
-def markoff_step(state, n_spatial=20):
+def markoff(state, n_spatial=20):
     """
     One step of markoff process
-    state: (n_spatial,)
-    return: (n_spatial,)
     """
-    for i in range(len(state)):
-        if i-1>=0 and i+1<=n_spatial-1:
-            if random.random()<1/3: # go left
-                state[i-1] += 1
-                state[i] -= 1
-            elif random.random()<1/3: # go right
-                state[i+1] += 1
-                state[i] -= 1
-        elif i==0:
-            if random.random()<1/3: # go left
-                state[i] -= 1
-            elif random.random()<1/3: # go right
-                state[i+1] += 1
-                state[i] -= 1
-        elif i==n_spatial-1:
-            if random.random()<1/3: # go left
-                state[i-1] += 1
-                state[i] -= 1
-            elif random.random()<1/3: # go right
-                state[i] -= 1
+    for i in range(len(state)): # different spatial position
+        for _ in range(int(state[i])): # different particles in the same position
+            r = random.random() # random number between 0 and 1, smaller than 1/3: left, larger than 2/3: right
+            if i!=0 and i!=n_spatial-1: # not boundary
+                if r<1/3: # left
+                    state[i] -= 1
+                    state[i-1] += 1
+                elif r>2/3: # right
+                    state[i] -= 1
+                    state[i+1] += 1
+            elif i==0: # left boundary, reflective
+                if r<1/3 or r>2/3:
+                    state[i] -= 1
+                    state[i+1] += 1
+            elif i==n_spatial-1:
+                if r<1/3 or r>2/3: # left
+                    state[i] -= 1
+                    state[i-1] += 1
     return state
 
-def markoff_step_reversal(state, n_spatial=20):
+def markoff_reversal(state, n_spatial=20):
     """
-    One step of markoff process
-    state: (n_spatial,)
-    return: (n_spatial,)
+    One step of markoff reversal process
     """
-    for i in range(len(state)):
-        if i-1>=0 and i+1<=n_spatial-1:
-            if random.random()<1/3: # left back
-                state[i-1] -= 1
-                state[i] += 1
-            elif random.random()<1/3: # right back
-                state[i+1] -= 1
-                state[i] += 1
-        elif i==0:
-            if random.random()<1/3: # left back
-                state[i] += 1
-            elif random.random()<1/3: # right back
-                state[i+1] -= 1
-                state[i] += 1
-        elif i==n_spatial-1:
-            if random.random()<1/3: # left back
-                state[i-1] -= 1
-                state[i] += 1
-            elif random.random()<1/3: # right back
-                state[i] += 1
+    for i in range(len(state)): # different spatial position
+        for _ in range(int(state[i])): # different particles in the same position
+            r = random.random() # random number between 0 and 1, smaller than 1/3: left, larger than 2/3: right
+            if i!=0 and i!=n_spatial-1: # not boundary
+                if r<1/3: # left
+                    state[i] -= 1
+                    state[i+1] += 1
+                elif r>2/3: # right
+                    state[i] -= 1
+                    state[i-1] += 1
+            elif i==0: # left boundary, reflective
+                if r<1/3 or r>2/3:
+                    state[i] -= 1
+                    state[i+1] += 1
+            elif i==n_spatial-1:
+                if r<1/3 or r>2/3: # left
+                    state[i] -= 1
+                    state[i-1] += 1
     return state
 
 def forward_process(initial_state, n_step):
@@ -89,9 +61,8 @@ def forward_process(initial_state, n_step):
     return: list of (n_spatial,)
     """
     state = initial_state
-    res = []
     for _ in range(n_step):
-        state = markoff_step(state)
+        state = markoff(state)
     return state
 
 def backward_process(initial_state, n_step):
@@ -103,47 +74,56 @@ def backward_process(initial_state, n_step):
     """
     state = initial_state
     for _ in range(n_step):
-        state = markoff_step_reversal(state)
+        state = markoff_reversal(state)
     return state
+
+def average_state(state):
+    """
+    Average state from a list of states
+    state_list: list of (n_spatial,)
+    return: (n_spatial,)
+    """
+    res = 0
+    for i in range(len(state)):
+        res += state[i]*i
+    return res/sum(state)
 
 
 if __name__ == "__main__":
-
-    # Forward process using Markoff chain
     n_spatial = 20
-    n_particle = 100
+    n_particle = 1000
     Forward_state = np.zeros(n_spatial)
     Forward_state[n_spatial//2] = n_particle
-    Backward_state = np.ones(n_spatial)*(n_particle/n_spatial)
+    Backward_state = np.ones(n_spatial)*int(n_particle/n_spatial)
+    # print(Backward_state)
 
-    # Forward process using Markoff chain
+    print("Forward diffusion process\n")
+    print("Forward initial average:",average_state(Forward_state))
     res1 = []
     res1.append(Forward_state)
+    temp = Forward_state.copy()
+    for _ in range(5):
+        temp = forward_process(temp, 5)
+        res1.append(temp)
+        print("state:",temp)
+        print("state average:",average_state(temp))
+
+    print("\nBackward anti-diffusion process\n")
+    print('Backward initial state:',temp)
+    print("Backward initial average:",average_state(temp))
     res2 = []
     for _ in range(5):
-        state = forward_process(Forward_state, 10)
-        res1.append(state)
+        temp = backward_process(temp, 50)
+        res2.append(temp)
+        print("state:",temp)
+        print("state average:",average_state(temp))
+
+    print("\nPure backward anti-diffusion process\n")
+    res3 = []
+    res3.append(Backward_state)
+    temp = Backward_state.copy()
     for _ in range(5):
-        state = backward_process(Backward_state, 10)
-        res2.append(state)
-
-    #画图
-    plt.figure()
-    for i in range(len(res1)):
-        plt.plot(res1[i],label=f'step {i+1}')
-    plt.xlim(0, n_spatial)
-    plt.title('Forward Process')
-    plt.xlabel('position')
-    plt.ylabel('number of particles')
-    plt.legend()
-    plt.savefig('./figure')
-
-    plt.figure()
-    for i in range(len(res2)):
-        plt.plot(res2[i],label=f'step {i+1}')
-    plt.xlim(0, n_spatial)
-    plt.title('Backward Process')
-    plt.xlabel('position')
-    plt.ylabel('number of particles')
-    plt.legend()
-    plt.savefig('./figure')
+        temp = backward_process(temp, 5)
+        res3.append(temp)
+        print("state:",temp)
+        print("state average:",average_state(temp))
