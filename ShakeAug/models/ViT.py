a32bef04d4f43586d7f60ee643f67afd50155071
tuyansheng <928235325@qq.com>
2026-02-10 13:41:51 +0800

    修改了ViT的错误

--- ShakeAug/models/ViT.py
+++ ShakeAug/models/ViT.py
@@ -1,26 +1,48 @@
 import torch
-from timm.models.vision_transformer import vit_small_patch16_224 as timm_vit_small_patch16_224
-from timm.models.vision_transformer import vit_tiny_patch16_224 as timm_vit_tiny_patch16_224
-from timm.models.vision_transformer import vit_small_patch32_224 as timm_vit_small_patch32_224
-from timm.models.vision_transformer import vit_base_patch16_224 as timm_vit_base_patch16_224
-from timm.models.vision_transformer import vit_base_patch32_224 as timm_vit_base_patch32_224
+from timm.models.vision_transformer import (
+    vit_tiny_patch16_224 as timm_vit_tiny_patch16_224,
+    vit_small_patch16_224 as timm_vit_small_patch16_224,
+    vit_small_patch32_224 as timm_vit_small_patch32_224,
+    vit_base_patch16_224 as timm_vit_base_patch16_224,
+    vit_base_patch32_224 as timm_vit_base_patch32_224,
+)
 
-def vit_tiny_patch16_224(num_classes: int, **kwargs):
-    model = timm_vit_tiny_patch16_224(pretrained=False, num_classes=num_classes)
+def vit_tiny_patch16_224(num_classes: int, img_size=32, **kwargs):
+    model = timm_vit_tiny_patch16_224(
+        pretrained=False,
+        num_classes=num_classes,
+        img_size=img_size,
+    )
     return model
 
-def vit_small_patch16_224(num_classes: int, **kwargs):
-    model = timm_vit_small_patch16_224(pretrained=False, num_classes=num_classes)
+def vit_small_patch16_224(num_classes: int, img_size=32, **kwargs):
+    model = timm_vit_small_patch16_224(
+        pretrained=False,
+        num_classes=num_classes,
+        img_size=img_size,
+    )
     return model
 
-def vit_small_patch32_224(num_classes: int, **kwargs):
-    model = timm_vit_small_patch32_224(pretrained=False, num_classes=num_classes)
+def vit_small_patch32_224(num_classes: int, img_size=32, **kwargs):
+    model = timm_vit_small_patch32_224(
+        pretrained=False,
+        num_classes=num_classes,
+        img_size=img_size,
+    )
     return model
 
-def vit_base_patch16_224(num_classes: int, **kwargs):
-    model = timm_vit_base_patch16_224(pretrained=False, num_classes=num_classes)
+def vit_base_patch16_224(num_classes: int, img_size=32, **kwargs):
+    model = timm_vit_base_patch16_224(
+        pretrained=False,
+        num_classes=num_classes,
+        img_size=img_size,
+    )
     return model
 
-def vit_base_patch32_224(num_classes: int, **kwargs):
-    model = timm_vit_base_patch32_224(pretrained=False, num_classes=num_classes)
+def vit_base_patch32_224(num_classes: int, img_size=32, **kwargs):
+    model = timm_vit_base_patch32_224(
+        pretrained=False,
+        num_classes=num_classes,
+        img_size=img_size,
+    )
     return model
